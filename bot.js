9c27fd9e688b45c3b17c53e071ac4211b735e8b3
Glitch (tilde) <none>
2018-03-08 20:37:27 +0000

    🖇🎫 Checkpoint .env:270800/11 bot.js:270800/42

--- bot.js
+++ bot.js
@@ -9,10 +9,50 @@ var util = require(__dirname + '/util/util.js');
 var bot = new Discord.Client();
 var prefix = "~";
 
-var pingCode = -1;
-
 var port = process.env.PORT || 3000;
 var token = process.env.TOKEN || -1;
+bot.login(token);
+
+bot.on('ready', async () =>
+{
+let icon = bot.user.avatarURL;
+    bot.setMaxListeners(15);
+    console.log("Connected to Discord!");
+    console.log("_____________________");
+    console.log('Bot is online - ' + bot.user.tag);
+    try
+    {
+        let link = await bot.generateInvite(["MANAGE_CHANNELS", "SEND_MESSAGES", "READ_MESSAGES"])
+        console.log("Invite: " + link);
+    }
+    catch (e)
+    {
+        console.log(e)
+    }
+
+    let guilds = bot.guilds.array().length;
+
+    bot.user.setStatus('online');
+
+    var store = [];
+    var oldf = console.log;
+    console.log = function()
+    {
+       store.push(arguments);
+       oldf.apply(console, arguments);
+    }
+
+    let users = bot.users.size;
+
+    console.log('Serving ' + users + ' users on ' + guilds + ' servers!');
+
+    bot.user.setActivity(`${bot.guilds.size} Servers | ~help`,
+    {
+        type: "SERVING"
+    })
+
+});
+
 
 app.use(express.static('public'));
 
