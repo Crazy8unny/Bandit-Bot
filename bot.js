c5b37be6c2e9f1ab1a6f1a8e3ea5971fa93aefd6
Glitch (tilde) <none>
2018-03-08 21:18:11 +0000

    📰🏠 Checkpoint bot.js:270800/776

--- bot.js
+++ bot.js
@@ -5,16 +5,18 @@ var util = require(__dirname + '/util/util.js');
 var bot = new Discord.Client();
 var prefix = "~";
 
+var botID = 421403753976037376;
+
 var token = process.env.TOKEN || -1;
 
-bot.on('ready', async () =>
+bot.on('ready', async function()
 {
     console.log("Connected to Discord!");
     console.log("_____________________");
     console.log('Bot is online - ' + bot.user.tag);
     try
     {
-        let link = await bot.generateInvite(["MANAGE_CHANNELS", "SEND_MESSAGES", "READ_MESSAGES"])
+        let link = await bot.generateInvite(["MANAGE_MESSAGES", "SEND_MESSAGES", "READ_MESSAGES"])
         console.log("Invite: " + link);
     }
     catch (e)
@@ -33,14 +35,34 @@ bot.on('ready', async () =>
 
 });
 
+bot.on("message", function (message) 
+{
+    if (!message.content.startsWith(prefix) && message.content.indexOf(botID) > 5 || !message.content.startsWith(prefix) && message.content.indexOf(botID) <= -1) return;
+    
+    let command = message.content.split(" ")[0].substr(1);
+    if (commands[command])
+    {
+        try
+        {
+            commands[command].run(message, message.content.split(" ").splice(1, 0));
+        }
+        catch (e)
+        {
+            
+        }
+    }
+});
+
 var commands = 
 {
     ping: {
         name: "Ping",
         description: "A simple command to check the latency of the bot.",
-        run: function()
+        usage: `${prefix}ping`,
+        run: function(message, args)
         {
-            
+            message.delete();
+            message.channel.send(`:ping_pong: Pong! \`${(new Date().getTime() - message.createdTimestamp)}ms\``).then(msg => {msg.delete(3000)});
         }
     }
 };
