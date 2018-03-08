0dd754fca9ce14e526d8813d06a47ab8659126e3
Glitch (tilde) <none>
2018-03-08 20:47:30 +0000

    🏑🚆 Checkpoint bot.js:270800/55 settings/configureation.json:270800/62 server.js:270800/122 package.json:270800/10

--- bot.js
+++ bot.js
@@ -1,7 +1,3 @@
-var express = require('express');
-var http = require("http");
-var app = express();
-
 var Discord = require('discord.js');
 
 var util = require(__dirname + '/util/util.js');
@@ -9,7 +5,6 @@ var util = require(__dirname + '/util/util.js');
 var bot = new Discord.Client();
 var prefix = "~";
 
-var port = process.env.PORT || 3000;
 var token = process.env.TOKEN || -1;
 bot.login(token);
 
@@ -53,39 +48,3 @@ let icon = bot.user.avatarURL;
 
 });
 
-
-app.use(express.static('public'));
-
-app.get("/", function(request, response)
-{
-    let datetime = new Date();
-    console.log("Ping recieved [" + util.formatShortDate(datetime) + ", " + util.formatShortTime(datetime) + "]");
-});
-
-app.get("/ping", function(request, response)
-{
-    let datetime = new Date();
-    let pcode = Math.floor(Math.random() * 10239571);
-    response.send(`Code: ${pcode}`);
-    console.log("Ping recieved [" + util.formatShortDate(datetime) + ", " + util.formatShortTime(datetime) + "]");
-});
-
-var listener = app.listen(port, function()
-{
-    console.log(process.env.PROJECT_DOMAIN.toUpperCase() + ' is online! [Port: ' + listener.address().port + ']');
-});
-
-// Keep server online
-setInterval(() =>
-{
-    var options = {
-        host: process.env.PROJECT_DOMAIN + ".glitch.me",
-        port: 80,
-        path: '/ping'
-    };
-
-    http.get(options, function(res) {}).on('error', function(e)
-    {
-        console.error("Got error: " + e.message);
-    });
-}, 280000);
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var http = require("http");
+var app = express();
+var util = require(__dirname + '/util/util.js');
+var child_process = require("child_process");
+
+child_process.fork(__dirname + "/bot.js");
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get("/", function(request, response)
+{
+    let datetime = new Date();
+    console.log("Ping recieved [" + util.formatShortDate(datetime) + ", " + util.formatShortTime(datetime) + "]");
+});
+
+app.get("/ping", function(request, response)
+{
+    let datetime = new Date();
+    let pcode = Math.floor(Math.random() * 10239571);
+    response.send(`Ping Code: ${pcode}`);
+    console.log("Ping recieved [" + util.formatShortDate(datetime) + ", " + util.formatShortTime(datetime) + "]");
+});
+
+var listener = app.listen(port, function()
+{
+    console.log(process.env.PROJECT_DOMAIN.toUpperCase() + ' is online! [Port: ' + listener.address().port + ']');
+});
+
+// Keep server online
+setInterval(() =>
+{
+    var options = {
+        host: process.env.PROJECT_DOMAIN + ".glitch.me",
+        port: 80,
+        path: '/ping'
+    };
+
+    http.get(options, function(res) {}).on('error', function(e)
+    {
+        console.error("Got error: " + e.message);
+    });
+}, 280000);
