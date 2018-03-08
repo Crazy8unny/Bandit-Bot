8bb231fe8b07536a8b3457fbb7505e68845da945
Glitch (tilde) <none>
2018-03-08 20:57:41 +0000

    👍🌴 Checkpoint bot.js:270800/199

--- bot.js
+++ bot.js
@@ -6,12 +6,9 @@ var bot = new Discord.Client();
 var prefix = "~";
 
 var token = process.env.TOKEN || -1;
-bot.login(token);
 
 bot.on('ready', async () =>
 {
-let icon = bot.user.avatarURL;
-    bot.setMaxListeners(15);
     console.log("Connected to Discord!");
     console.log("_____________________");
     console.log('Bot is online - ' + bot.user.tag);
@@ -27,24 +24,25 @@ let icon = bot.user.avatarURL;
 
     let guilds = bot.guilds.array().length;
 
-    bot.user.setStatus('online');
-
-    var store = [];
-    var oldf = console.log;
-    console.log = function()
-    {
-       store.push(arguments);
-       oldf.apply(console, arguments);
-    }
-
-    let users = bot.users.size;
-
-    console.log('Serving ' + users + ' users on ' + guilds + ' servers!');
+    bot.user.setStatus('idle');
 
     bot.user.setActivity(`${bot.guilds.size} Servers | ~help`,
     {
-        type: "SERVING"
+        type: "WATCHING"
     })
 
 });
 
+var commands = 
+{
+    ping: {
+        name: "Ping",
+        description: "A simple command to check the latency of the bot.",
+        run: function()
+        {
+            
+        }
+    }
+};
+
+bot.login(token);
