5c641af13741a2b31e0721ec43ba493296b26003
Glitch (tilde) <none>
2018-03-16 07:21:21 +0000

    📰🌮 Checkpoint ./public/client.js:270800/1675 ./public/style.scss:270800/19

--- public/client.js
+++ public/client.js
@@ -1,7 +1,34 @@
+var menuOpen = false;
+
 $(document).ready(function()
 {
     $(".navigation-icon").click(function()
     {
-        $(".navigation").animate("width", "90%", 2000);
+        if (!menuOpen)
+        {
+            $(".navigation").animate({
+                  left: 0,
+                  opacity: 1
+              }, 1000, "linear");
+
+            $(this).animate({
+                  left: $(".navigation").width() + 15
+              }, 1000, "linear");
+          
+            menuOpen = true;
+        }
+        else
+        {
+            $(".navigation").animate({
+                  left: -$(".navigation").width(),
+                  opacity: 0
+              }, 1000, "linear");
+
+            $(this).animate({
+                  left: 15
+              }, 1000, "linear");
+          
+            menuOpen = false;
+        }
     });
 });
