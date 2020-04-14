ea71bed080149dffb411953982cb725dc6a2a201
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-14 17:30:20 +0300

    Add choose option

--- commands/choose.js
+++ commands/choose.js
@@ -12,9 +12,15 @@ class Choose extends Command {
   }
 
   async run (message, args, level) { 
-    this.client.logger.log(message.toString());
-    this.client.logger.log(args.toString());
-    this.client.logger.log(level.toString());
+    let optionsString = msg.substring(3, msg.length);
+    let options = optionsString.split(' או ');
+    if (options.length > 1) {
+        res = options[Math.floor(Math.random() * options.length)];
+    }
+    else {
+        res = "אתה צריך לשלוח מספר אפשרויות עם `או` מפריד ביניהם אחינו"
+    }
+    message.channel.send(res.toString());
   }
 }
 
