d5fcec040931cc9bbd9503c4107c05a1637d9960
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-14 17:36:56 +0300

    fix

--- commands/choose.js
+++ commands/choose.js
@@ -12,7 +12,7 @@ class Choose extends Command {
   }
 
   async run (message, args, level) { 
-    let optionsString = message.substring(3, msg.length);
+    let optionsString = message.substring(3, message.length);
     let options = optionsString.split(' או ');
     if (options.length > 1) {
         res = options[Math.floor(Math.random() * options.length)];
