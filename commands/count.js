5a027a111d20d89e5272a6281496648004672983
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-14 16:44:45 +0300

    Add count

---
+++ commands/count.js
@@ -0,0 +1,36 @@
+const Command = require("../base/Command.js");
+
+class Count extends Command {
+  constructor (client) {
+    super(client, {
+      name: "ספור",
+      description: "סופר לאחור",
+      usage: "ספור",
+      category: "שימושי",
+      aliases: []
+    });
+  }
+
+  async run (message, args, level) { 
+    let countdown = args[0];
+    let printLion = true;
+    if (!args[0] || countdown == 5) {
+      countdown = 5;
+      printLion = true;
+    }
+    if (countdown.toString() == 'NaN' || countdown > 100 || countdown < 0) {
+        countdown = 0;
+        message.channel.send('עליך לציין מספר שניות לאחר הפקודה (עד 100 שניות)');
+    }
+    while (countdown > 0) {
+      message.channel.send(countdown.toString());
+      await this.client.wait(1000);
+      countdown--;
+    }
+    if (printLion) {
+      message.channel.send(':lion_face:');
+    }
+  }
+}
+
+module.exports = Count;
--- events/ready.js
+++ events/ready.js
@@ -28,7 +28,7 @@ module.exports = class {
 
     // Set the game as the default help command + guild count.
     // NOTE: This is also set in the guildCreate and guildDelete events!
-    this.client.user.setActivity(`LF2 | ${this.client.settings.get("default").prefix}help`);
+    this.client.user.setActivity(`LF2 | עזרה${this.client.settings.get("default").prefix}`);
   
     // Log that we're ready to serve, so we know the bot accepts commands.
     this.client.logger.log(`${this.client.user.tag}, ready to serve ${this.client.users.cache.size} users in ${this.client.guilds.cache.size} servers.`, "ready");
