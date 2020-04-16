06da5b661ffc6dc3da88b659eee39f4427116b56
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 23:30:16 +0300

    rter

--- commands/avatar.js
+++ commands/avatar.js
@@ -15,8 +15,8 @@ class Avatar extends Command {
   }
 
   async run (message, args, level) {
-    const image = require('../assets/basePhoto.png').toBuffer();
-    sharp(image).resize(200, 200).toBuffer().then(data => {
+    // const image = require('../assets/basePhoto.png').toBuffer();
+    sharp('../assets/basePhoto.jpg').resize(200, 200).toBuffer().then(data => {
       message.channel.send(data);
       message.channel.send("בדיקה");
     }).catch(err => {
