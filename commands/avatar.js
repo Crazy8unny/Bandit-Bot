f31b289020315d79a3b483153ba049a18791333d
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 20:35:36 +0300

    start avatar command and add sharp libary

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,5 @@
 const Command = require("../base/Command.js");
+const sharp = require('sharp');
 
 class Avatar extends Command {
   constructor (client) {
@@ -13,7 +14,8 @@ class Avatar extends Command {
   }
 
   async run (message, args, level) { 
-	
+    let embed = new Discord.RichEmbed().setImage(message.author.avatarURL).setColor('#275BF0');
+    message.channel.send(embed);
   }
 }
 
