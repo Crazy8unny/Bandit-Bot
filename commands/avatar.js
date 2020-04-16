806aa0327d8cf1c5bfa312ab9e01ebd1250b7240
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 19:09:17 +0300

    Test add avatar modules

---
+++ commands/avatar.js
@@ -0,0 +1,20 @@
+const Command = require("../base/Command.js");
+
+class Avatar extends Command {
+  constructor (client) {
+    super(client, {
+      name: "אווטאר",
+      description: "הופך אותך לבאנדיט אפל",
+      usage: "אווטאר",
+      category: "שימושי",
+      aliases: [],
+      permLevel: "Bot Owner"
+    });
+  }
+
+  async run (message, args, level) { 
+	
+  }
+}
+
+module.exports = Avatar;
