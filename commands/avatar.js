045a60f44d8084c0b0e4f2057ce1a218d7ece377
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 22:41:42 +0300

    asdf

--- commands/avatar.js
+++ commands/avatar.js
@@ -17,7 +17,7 @@ class Avatar extends Command {
   async run (message, args, level) {
     let member = message.mentions.users.first() || message.author; 
     // let embed = new Discord.MessageEmbed().setImage(member.displayAvatarURL()).setAuthor( message.author).setColor('#1E2023');
-    sharp('http://cdn.discordapp.com/avatars/300324644932681728/c1d5f3c1596b2901b57fc1f62f8ab785.png,)
+    sharp('http://cdn.discordapp.com/avatars/300324644932681728/c1d5f3c1596b2901b57fc1f62f8ab785.png')
     .composite([{ input: 'http://cdn.discordapp.com/avatars/300332593881153547/3d0dced924f5a031779f88cbb56d300f.png'}])
     .toBuffer()
     .then(function(outputBuffer) {
