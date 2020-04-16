67d9e0bc179bdb844136af2f610481e6886909bf
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 22:14:19 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -17,9 +17,8 @@ class Avatar extends Command {
   async run (message, args, level) {
     let member = message.mentions.users.first() || message.author; 
     // let embed = new Discord.MessageEmbed().setImage(member.displayAvatarURL()).setAuthor( message.author).setColor('#1E2023');
-    sharp("../assets/basePhoto.png").composite([{ input: member.displayAvatarURL()}])
-    .toBuffer({ resolveWithObject: true })
-    .then(({ data, info }) => {
+    sharp("../assets/basePhoto.png").composite([{ input: "https://lf2.co.il/NewImages/My/Ayalx/RasahrGuy2M.png"}])
+    .then(function(outputBuffer) {
       // let embed = new Discord.MessageEmbed().setImage(data).setAuthor( message.author).setColor('#1E2023');
       message.channel.send("בדיקה");    
       // message.channel.send(embed);    
