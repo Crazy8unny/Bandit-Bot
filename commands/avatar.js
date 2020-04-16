ca7ce0ffe29a80fdff438d20872286f95c57484e
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 22:56:13 +0300

    asdf

--- commands/avatar.js
+++ commands/avatar.js
@@ -15,20 +15,25 @@ class Avatar extends Command {
   }
 
   async run (message, args, level) {
-    let member = message.mentions.users.first() || message.author; 
-    // let embed = new Discord.MessageEmbed().setImage(member.displayAvatarURL()).setAuthor( message.author).setColor('#1E2023');
-    sharp('http://cdn.discordapp.com/avatars/300324644932681728/c1d5f3c1596b2901b57fc1f62f8ab785.png')
-    .composite([{ input: 'http://cdn.discordapp.com/avatars/300332593881153547/3d0dced924f5a031779f88cbb56d300f.png'}])
-    .toBuffer()
-    .then(function(outputBuffer) {
-      // let embed = new Discord.MessageEmbed().setImage(data).setAuthor( message.author).setColor('#1E2023');
-      message.channel.send("בדיקה");    
-      // message.channel.send(outputBuffer);    
-    })
-    .catch(err => {
-      message.channel.send(" 2בדיקה");    
-      message.channel.send(err.toString());    
-    });
+    // let member = message.mentions.users.first() || message.author; 
+    // // let embed = new Discord.MessageEmbed().setImage(member.displayAvatarURL()).setAuthor( message.author).setColor('#1E2023');
+    // sharp('http://cdn.discordapp.com/avatars/300324644932681728/c1d5f3c1596b2901b57fc1f62f8ab785.png')
+    // .composite([{ input: 'http://cdn.discordapp.com/avatars/300332593881153547/3d0dced924f5a031779f88cbb56d300f.png'}])
+    // .toBuffer()
+    // .then(function(outputBuffer) {
+    //   // let embed = new Discord.MessageEmbed().setImage(data).setAuthor( message.author).setColor('#1E2023');
+    //   message.channel.send("בדיקה");    
+    //   // message.channel.send(outputBuffer);    
+    // })
+    // .catch(err => {
+    //   message.channel.send(" 2בדיקה");    
+    //   message.channel.send(err.toString());    
+    // });
+    const input = ('../assets/basePhoto.png');
+    const composite = ('../assets/deep_f2.png');
+    const output = await sharp(input).composite({ input: composite }).png().toBuffer();
+    message.channel.send("בדיקה");    
+    message.channel.send(output);    
   }
 }
 
