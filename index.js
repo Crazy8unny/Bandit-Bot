76c32a9b7cf05da97dd5671fad39308b1a2189c1
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 15:28:11 +0300

    test

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ class BanditBot extends Client {
       props.init(this);
     }
     this.containsCommands.set(props.help.name, props);
-    props.conf.contains.forEach(alias => {
+    props.help.contains.forEach(alias => {
       this.containsCommands.set(alias, props.help.name);
     });
     return false;
