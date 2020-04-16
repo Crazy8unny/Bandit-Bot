faae81cb965fe55f1172f955d5cba62389555760
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-16 16:17:45 +0300

    Fix seemingly

--- containsCommands/seemingly.js
+++ containsCommands/seemingly.js
@@ -3,16 +3,30 @@ const ContainsCommand = require("../base/ContainsCommand.js");
 class Seemingly extends ContainsCommand {
   constructor (client) {
     super(client, {
-      name: "לכאדורה",
+      name: "לכאורה",
       description: "אין פה הוצאת דיבה",
-      contains: ["לכאורה"]
+      contains: ["לכאורה", "טיפש", "טיפשה", "מטומטם", "מטומטמת", "כלב", "כלבה", "חרא", "קקי", "אידיוט", "אידיוטית", "מפגר"
+                , "מפגרת", "סתום", "סתומה", "דביל", "דבילית", "שמן", "שמנה", "מכוער", "מכוערת", "בן זונה", "בת זונה"],
+      category: "מילים מוכלות"
     });
   }
 
   async run (message, args, level) {
-    let responses =  [];
-    responses.push("אין פה הוצאת דיבה !"); 
-    message.channel.send(responses[0]);
+    let res = "אין פה הוצאת דיבה !";
+    let curses = ["טיפש", "טיפשה", "מטומטם", "מטומטמת", "כלב", "כלבה", "חרא", "קקי", "אידיוט", "אידיוטית", "מפגר"
+    , "מפגרת", "סתום", "סתומה", "דביל", "דבילית", "שמן", "שמנה", "מכוער", "מכוערת", "בן זונה", "בת זונה"];
+    let seemingly = "לכאורה"
+    let msg = message.toString();
+    let curse = curses.some(substring=>msg.includes(substring));
+    if (curse) {
+        if (msg.contains(seemingly)) {
+            res = "איזה מלך שאמרת לכאורה !"
+            message.react("👑");
+        } else {
+            res = "תגיד לכאורה על זה, אתה חייב להגיד !"
+        }
+    }
+    message.channel.send(res);
   }
 }
 
