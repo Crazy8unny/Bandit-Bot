4edad05c5876d91c994233bb623b11bc8cc34c60
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-17 12:51:45 +0300

    test

--- util/utils.js
+++ util/utils.js
@@ -1,17 +1,20 @@
-export function getImage(url) {
-    let options = {
-        url: url,
-        method: "get",
-        encoding: null
-    };
-    return (request(options, function (error, response, body) {
-        if (error) {
-            console.error('error:', error);
-        }
-        else {
-            console.log('Response: StatusCode:', response && response.statusCode);
-            console.log('Response: Body: Length: %d. Is buffer: %s', body.length, (body instanceof Buffer));
-            return body;
-        }
-    }));
+class utils {
+    getImage(url) {
+        let options = {
+            url: url,
+            method: "get",
+            encoding: null
+        };
+        return(request(options, function (error, response, body) {
+            if (error) {
+                console.error('error:', error);
+            } else {
+                console.log('Response: StatusCode:', response && response.statusCode);
+                console.log('Response: Body: Length: %d. Is buffer: %s', body.length, (body instanceof Buffer));
+                return body;
+            }
+        }));
+    }
 }
+
+module.exports = utils;
