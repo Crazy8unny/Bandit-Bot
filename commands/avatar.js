160285521b885fc3ba469c0a67ada4b535d63658
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-17 13:43:34 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -17,11 +17,10 @@ class Avatar extends Command {
 
   async run (message, args, level) {
     let member = message.mentions.users.first() || message.author; 
-    const basePhoto = util.getImage("https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png");
-    const avatar = util.getImage(member.displayAvatarURL());
     // let embed = new Discord.MessageEmbed().setImage().setAuthor(message.author.username, message.author.avatar).setColor('#1E2023');
     // message.channel.send(embed);
-    sharp(avatar).composite([{ input: basePhoto}]).toBuffer()
+    sharp(util.getImage("https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png"))
+    .composite([{ input: util.getImage(member.displayAvatarURL())}]).toBuffer()
     .then(function(outputBuffer) {
       // let embed = new Discord.MessageEmbed().setImage(data).setAuthor( message.author).setColor('#1E2023');
       message.channel.send("בדיקה");    
