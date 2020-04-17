32731ed0875de49b775f7d5cee6f99c2b62b0715
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-17 12:45:43 +0300

    test

--- util/utils.js
+++ util/utils.js
@@ -1,13 +1,14 @@
-function getImage (url) {
+export function getImage(url) {
     let options = {
         url: url,
         method: "get",
         encoding: null
     };
-    return(request(options, function (error, response, body) {
+    return (request(options, function (error, response, body) {
         if (error) {
             console.error('error:', error);
-        } else {
+        }
+        else {
             console.log('Response: StatusCode:', response && response.statusCode);
             console.log('Response: Body: Length: %d. Is buffer: %s', body.length, (body instanceof Buffer));
             return body;
