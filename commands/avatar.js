4bbe217b47b4a9887f14afee35db1a296521fef4
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-17 12:34:31 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,7 @@
 const Command = require("../base/Command.js");
 const sharp = require('sharp');
 const Discord = require('discord.js');
+const util = require('../util/utils');
 
 class Avatar extends Command {
   constructor (client) {
@@ -16,11 +17,11 @@ class Avatar extends Command {
 
   async run (message, args, level) {
     let member = message.mentions.users.first() || message.author; 
-    const basePhoto = "https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png";
-    const avatar = member.displayAvatarURL();
+    const basePhoto = util.getImage("https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png");
+    const avatar = util.getImage(member.displayAvatarURL());
     // let embed = new Discord.MessageEmbed().setImage().setAuthor(message.author.username, message.author.avatar).setColor('#1E2023');
     // message.channel.send(embed);
-    sharp(basePhoto).composite([{ input: avatar}]).toBuffer()
+    sharp(avatar).composite([{ input: basePhoto}]).toBuffer()
     .then(function(outputBuffer) {
       // let embed = new Discord.MessageEmbed().setImage(data).setAuthor( message.author).setColor('#1E2023');
       message.channel.send("בדיקה");    
