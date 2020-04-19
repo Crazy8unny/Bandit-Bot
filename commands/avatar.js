73d8d9c5978261f0bfbff9a8bbe2e75db1eb4b5a
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-19 23:11:26 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -16,10 +16,10 @@ class Avatar extends Command {
   }
 
   async run(message, args, level) {
-    let inputBuffer = Buffer.from(util.request({ url: "https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png", method: "get" }));
+    let inputBuffer = util.request({ url: "https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png", method: "get", encoding: null });
     sharp(inputBuffer)
       .resize(160, 160)
-      .toBuffer()
+      .toBuffer({ resolveWithObject: true })
       .then(data => {
         let embed = new Discord.MessageEmbed()
           .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: data }])
