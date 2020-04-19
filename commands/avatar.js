20909d579c1f3bc9927605e881d23316a0f44ace
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-20 00:28:18 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -16,30 +16,30 @@ class Avatar extends Command {
   }
 
   async run(message, args, level) {
-    let member = message.mentions.users.first() || message.author; 
+    let member = message.mentions.users.first() || message.author;
     let basePhoto = "https://cdn.discordapp.com/attachments/699235141134057492/701537065745121382/basePhoto.png";
-    
+
     let options = {
       url: "http://image-merger.herokuapp.com/api/v1.0/",
       method: "Post",
       json: {
         "foreground_url": basePhoto,
-        "background_url": "https://cdn.discordapp.com/avatars/"+ member.id +  "/" + member.avatar +".png"
+        "background_url": "https://cdn.discordapp.com/avatars/" + member.id + "/" + member.avatar + ".png"
       }
     }
 
-    console.log("https://cdn.discordapp.com/avatars/"+ member.id +  "/" + member.avatar +".png")
-    let response = util.request(options);
-    console.log(response);
-    
-    message.channel.send("ברוך הבא לגאנג אח שלי");
-    let embed = new Discord.MessageEmbed()
-      .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: response[0].url }])
-      .setImage('attachment://' + 'Bandit' + member.username + '.png')
-      .setAuthor(message.author.username, message.author.displayAvatarURL())
-      .setDescription("ברוך הבא לגאנג אח שלי")
-      .setColor('#1E2023');
-    message.channel.send(embed);
+    console.log("https://cdn.discordapp.com/avatars/" + member.id + "/" + member.avatar + ".png")
+    util.request(options).then(respons => {
+      console.log(response);
+      message.channel.send("ברוך הבא לגאנג אח שלי");
+      let embed = new Discord.MessageEmbed()
+        .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: response[0].url }])
+        .setImage('attachment://' + 'Bandit' + member.username + '.png')
+        .setAuthor(message.author.username, message.author.displayAvatarURL())
+        .setDescription("ברוך הבא לגאנג אח שלי")
+        .setColor('#1E2023');
+      message.channel.send(embed);
+    })
   }
 }
 
