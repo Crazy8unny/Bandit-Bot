a1f05869524473e3fde2c2f50bf00b8d17a915fb
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-19 19:21:57 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const util = require('../util/utils.js');
 
 class Avatar extends Command {
-  constructor (client) {
+  constructor(client) {
     super(client, {
       name: "אווטאר",
       description: "הופך אותך לבאנדיט אפל",
@@ -15,28 +15,30 @@ class Avatar extends Command {
     });
   }
 
-  async run (message, args, level) {
-    const semiTransparentRedPng = await sharp(Buffer.from(util.request(
-      {url: "https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png", method: "get", encoding: null}), 'base64'))
-      .resize({width: 200, height: 200})
+  async run(message, args, level) {
+    let semiTransparentRedPng;
+    await sharp(Buffer.from(util.request(
+      { url: "https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png", method: "get", encoding: null }), 'base64'))
+      .resize({ width: 200, height: 200 })
       .png()
-      .toBuffer();
-      
+      .toBuffer()
+      .then(image => {
+        semiTransparentRedPng = image;
+        let embed = new Discord.MessageEmbed()
+          .attachFiles([{ name: "image.png", attachment: semiTransparentRedPng }])
+          .setImage('attachment://image.png')
+          .setAuthor(message.author.username, message.author.displayAvatarURL())
+          .setColor('#1E2023');
+        message.channel.send(embed);
+      })
+
     //   let options = {
     //     url: url,
     //     method: "get",
     //     encoding: null
     // };
-      // const basePhoto = util.getImage("https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png")
-      // const photo = util.getImage("https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png");
-    
-
-      let embed = new Discord.MessageEmbed()
-      .attachFiles([{name: "image.png", attachment:semiTransparentRedPng}])
-      .setImage('attachment://image.png')
-      .setAuthor(message.author.username, message.author.displayAvatarURL())
-      .setColor('#1E2023');
-      message.channel.send(embed);
+    // const basePhoto = util.getImage("https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png")
+    // const photo = util.getImage("https://cdn.discordapp.com/attachments/699235141134057492/700626850983968768/basePhoto.png");
   }
 }
 
