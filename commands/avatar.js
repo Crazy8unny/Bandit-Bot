b8c109dfdd0cfd465e0943ca2353d60212727f83
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-20 00:08:41 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -34,7 +34,7 @@ class Avatar extends Command {
 
     message.channel.send("ברוך הבא לגאנג אח שלי");
     let embed = new Discord.MessageEmbed()
-      .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: response.url }])
+      .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: response.output_image.url }])
       .setImage('attachment://' + 'Bandit' + member.username + '.png')
       .setAuthor(message.author.username, message.author.displayAvatarURL())
       .setDescription("ברוך הבא לגאנג אח שלי")
--- util/utils.js
+++ util/utils.js
@@ -6,9 +6,9 @@ class utils {
             if (error) {
                 console.error('error:', error);
             } else {
-                console.log('Response: StatusCode:', response && response.statusCode);
-                console.log('Response: Body: Length: %d. Is buffer: %s', body.length, (body instanceof Buffer));
-                console.log(body);
+                // console.log('Response: StatusCode:', response && response.statusCode);
+                // console.log('Response: Body: Length: %d. Is buffer: %s', body.length, (body instanceof Buffer));
+                // console.log(body);
                 return body;
             }
         }));
