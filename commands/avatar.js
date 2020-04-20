f8e9459b74975920b3c722b9a554128aa5af607b
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-20 19:58:09 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,6 +2,7 @@ const Command = require("../base/Command.js");
 const sharp = require('sharp');
 const Discord = require('discord.js');
 const util = require('../util/utils.js');
+const jimp = require('jimp');
 
 class Avatar extends Command {
   constructor(client) {
@@ -18,28 +19,30 @@ class Avatar extends Command {
   async run(message, args, level) {
     let member = message.mentions.users.first() || message.author; 
     let basePhoto = "https://cdn.discordapp.com/attachments/699235141134057492/701537065745121382/basePhoto.png";
-    
-    let options = {
-      url: "http://image-merger.herokuapp.com/api/v1.0/",
-      method: "Post",
-      json: {
-        "foreground_url": basePhoto,
-        "background_url": "https://cdn.discordapp.com/avatars/"+ member.id +  "/" + member.avatar +".png"
-      }
-    }
 
-    console.log("https://cdn.discordapp.com/avatars/"+ member.id +  "/" + member.avatar +".png")
-    let response = util.request(options);
-    console.log(response);
+    jimp.read("../assets/basePhoto.png").then(image => {
+      message.channel.send("ברוך הבא לגאנג אח שלי");
+      let embed = new Discord.MessageEmbed()
+        .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: image }])
+        .setImage('attachment://' + 'Bandit' + member.username + '.png')
+        .setAuthor(message.author.username, message.author.displayAvatarURL())
+        .setDescription("ברוך הבא לגאנג אח שלי")
+        .setColor('#1E2023');
+      message.channel.send(embed);
+    })
     
-    message.channel.send("ברוך הבא לגאנג אח שלי");
-    let embed = new Discord.MessageEmbed()
-      .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: response.output_photo.url }])
-      .setImage('attachment://' + 'Bandit' + member.username + '.png')
-      .setAuthor(message.author.username, message.author.displayAvatarURL())
-      .setDescription("ברוך הבא לגאנג אח שלי")
-      .setColor('#1E2023');
-    message.channel.send(embed);
+    // let options = {
+    //   url: "http://image-merger.herokuapp.com/api/v1.0/",
+    //   method: "Post",
+    //   json: {
+    //     "foreground_url": basePhoto,
+    //     "background_url": "https://cdn.discordapp.com/avatars/"+ member.id +  "/" + member.avatar +".png"
+    //   }
+    // }
+
+    // console.log("https://cdn.discordapp.com/avatars/"+ member.id +  "/" + member.avatar +".png")
+    // let response = util.request(options);
+    // console.log(response);
   }
 }
 
