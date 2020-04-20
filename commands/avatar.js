3c3522ca483ab9b96888eb87856bd8ad750beaf4
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-20 20:04:27 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -20,7 +20,7 @@ class Avatar extends Command {
     let member = message.mentions.users.first() || message.author; 
     let basePhoto = "https://cdn.discordapp.com/attachments/699235141134057492/701537065745121382/basePhoto.png";
 
-    jimp.read("../assets/basePhoto.png").then(image => {
+    jimp.read(basePhoto).then(image => {
       message.channel.send("ברוך הבא לגאנג אח שלי");
       let embed = new Discord.MessageEmbed()
         .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: image }])
