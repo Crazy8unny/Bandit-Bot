195447d6c45806d68dc91d932e7f963af52c4f06
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-28 22:42:26 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -20,20 +20,9 @@ class Avatar extends Command {
     let member = message.mentions.users.first() || message.author; 
     let basePhoto = "https://cdn.discordapp.com/attachments/699235141134057492/701537065745121382/basePhoto.png";
     
-    Jimp.read('https://www.israelhayom.co.il/sites/default/files/styles/566x349/public/images/articles/2016/05/04/14623178569657_b.jpg')
+    Jimp.read(basePhoto)
     .then(image => {
-      // message.channel.send("ברוך הבא לגאנג אח שלי");
-      // let embed = new Discord.MessageEmbed()
-      //   .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: image }])
-      //   .setImage('attachment://' + 'Bandit' + member.username + '.png')
-      //   .setAuthor(message.author.username, message.author.displayAvatarURL())
-      //   .setDescription("ברוך הבא לגאנג אח שלי")
-      //   .setColor('#1E2023');
-      // message.channel.send(embed);
       image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-        console.log(buffer)
-        console.log("##############")
-      message.channel.send("ברוך הבא לגאנג אח שלי");
       let embed = new Discord.MessageEmbed()
         .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: buffer }])
         .setImage('attachment://' + 'Bandit' + member.username + '.png')
