1ed1ac8a645ff21f5362defeda5573b070a24c46
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-28 18:42:29 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -22,15 +22,18 @@ class Avatar extends Command {
     
     Jimp.read('https://www.israelhayom.co.il/sites/default/files/styles/566x349/public/images/articles/2016/05/04/14623178569657_b.jpg')
     .then(image => {
-      console.log(image);
       message.channel.send("ברוך הבא לגאנג אח שלי");
-      let embed = new Discord.MessageEmbed()
-        // .attachFiles([{ name: 'Bandit' + member.username + '.jpg', attachment: image }])
-        // .setImage('attachment://' + 'Bandit' + member.username + '.jpg')
-        .setAuthor(message.author.username, message.author.displayAvatarURL())
-        .setDescription("ברוך הבא לגאנג אח שלי")
-        .setColor('#1E2023');
-      message.channel.send(embed);
+      // let embed = new Discord.MessageEmbed()
+      //   .attachFiles([{ name: 'Bandit' + member.username + '.png', attachment: image }])
+      //   .setImage('attachment://' + 'Bandit' + member.username + '.png')
+      //   .setAuthor(message.author.username, message.author.displayAvatarURL())
+      //   .setDescription("ברוך הבא לגאנג אח שלי")
+      //   .setColor('#1E2023');
+      // message.channel.send(embed);
+      bot.uploadFile({
+        to: channelID,
+        file: image
+      }).catch(console.error);
     })
     .catch(err => {
       console.log(err);
