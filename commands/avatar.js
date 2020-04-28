bbdcea5a5be90f1af662880c4ad50d6f4bb40299
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-28 16:38:40 +0300

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,7 @@ const Command = require("../base/Command.js");
 const sharp = require('sharp');
 const Discord = require('discord.js');
 const util = require('../util/utils.js');
-const jimp = require('jimp');
+const Jimp = require('jimp');
 
 class Avatar extends Command {
   constructor(client) {
@@ -20,7 +20,7 @@ class Avatar extends Command {
     let member = message.mentions.users.first() || message.author; 
     let basePhoto = "https://cdn.discordapp.com/attachments/699235141134057492/701537065745121382/basePhoto.png";
     
-    Jimp.read('http://www.example.com/path/to/lenna.jpg')
+    Jimp.read('https://www.israelhayom.co.il/sites/default/files/styles/566x349/public/images/articles/2016/05/04/14623178569657_b.jpg')
     .then(image => {
       message.channel.send("ברוך הבא לגאנג אח שלי");
       let embed = new Discord.MessageEmbed()
@@ -34,6 +34,7 @@ class Avatar extends Command {
     .catch(err => {
       console.log(err);
     });
+
     
     // let options = {
     //   url: "http://image-merger.herokuapp.com/api/v1.0/",
