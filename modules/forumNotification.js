06ebe91c19e998065c8ba10787843d0916df9bcb
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-29 17:22:51 +0300

    test

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -7,12 +7,25 @@ const Discord = require('discord.js');
 const util = require('../util/utils');
 
 class ForumNotification {
-  static listen (lastThread) {
+  static listen(lastThread) {
     const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
     // console.log(timestamp); 
     const prevName = lastThread.get("name");
     // console.log("test name: " + prevName);
-    console.log(util.request({ url: "https://lf2.co.il/forum/index.php", method: "get"}));
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://lf2.co.il/forum/index.php",
+      "method": "GET",
+      "headers": {
+        "cache-control": "no-cache",
+        "Content-Type": "text/html; charset=iso-8859-8"
+      },
+      "beforeSend": function (jqXHR) {
+        jqXHR.overrideMimeType('text/html;charset=iso-8859-8');
+      }
+    }
+    console.log(util.request(settings));
   }
 }
 
