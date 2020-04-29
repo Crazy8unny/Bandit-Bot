8f9641e17daade89f40a11e0f449c0e52e7fe8e4
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-04-29 17:15:48 +0300

    test

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -4,13 +4,15 @@ Logger class for easy and aesthetically pleasing console logging
 const chalk = require("chalk");
 const moment = require("moment");
 const Discord = require('discord.js');
+const util = require('../util/utils');
 
 class ForumNotification {
   static listen (lastThread) {
     const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
     // console.log(timestamp); 
     const prevName = lastThread.get("name");
-    console.log("test name: " + prevName);
+    // console.log("test name: " + prevName);
+    console.log(util.request({ url: "https://lf2.co.il/forum/index.php", method: "get"}));
   }
 }
 
