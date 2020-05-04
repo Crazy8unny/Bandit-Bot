e519b3a15e0c2d8dfa36b868352a664716e3a4d0
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-04 21:52:06 +0300

    Bameranggggg

---
+++ commands/bamerang.js
@@ -0,0 +1,26 @@
+const Command = require("../base/Command.js");
+
+class Bamerang extends Command {
+  constructor (client) {
+    super(client, {
+      name: "באמרנג",
+      description: "באמרנגגגגגג",
+      usage: "באמרנג",
+      aliases: [],
+      category: "פנאי"
+    });
+  }
+
+  async run (message, args, level) {
+    try {
+        let embed = new Discord.MessageEmbed()
+        .setImage("https://i.imgur.com/2CPxx0k.gif")
+        .setDescription("<@" + message.author.id + ">" + " באמרנגגגגגג")
+        .setColor('#1E2023');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Bamerang;
