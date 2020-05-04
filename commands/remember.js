11321492f19d6606023b2b608150b19cc1286c5e
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-04 21:13:20 +0300

    fix

--- commands/remember.js
+++ commands/remember.js
@@ -12,25 +12,25 @@ class Remember extends Command {
     }
 
     async run(message, args, level) {
-        // let msg = message.toString();
-        // let res;
-        // let settings = this.client.getSettings(message.guild);
-        // if (msg.includes("אנחנו אנחנו")) {
-        //     res = "אנחנו אנחנו ? מה זה בכלל ?";
-        // }
-        // else if (args[0] == "אנחנו" && args[1] == null) {
-        //     res = "אתם  ב" + settings.where;
-        // }
-        // else if (args[0].startsWith('ב') && msg.includes("אנחנו ב")) {
-        //     let position = msg.search("אנחנו ב");
-        //     let placeString = msg.substring(position + 7, msg.length);
-        //     settings.where = placeString;
-        //     this.client.writeSettings(message.guild.id, settings);
-        // }
-        // else {
-        //     res = "שימוש שגוי בפקודה, שלח`אנחנו ב` או `איפה אנחנו``>"
-        // }
-        // message.channel.send(res.toString());
+        let msg = message.toString();
+        let res;
+        let settings = this.client.getSettings(message.guild);
+        if (msg.includes("אנחנו אנחנו")) {
+            res = "אנחנו אנחנו ? מה זה בכלל ?";
+        }
+        else if (args[0] == "אנחנו" && args[1] == null) {
+            res = "אתם  ב" + settings.where;
+        }
+        else if (args[0].startsWith('ב') && msg.includes("אנחנו ב")) {
+            let position = msg.search("אנחנו ב");
+            let placeString = msg.substring(position + 7, msg.length);
+            settings.where = placeString;
+            this.client.writeSettings(message.guild.id, settings);
+        }
+        else {
+            res = "שימוש שגוי בפקודה, שלח`אנחנו ב` או `איפה אנחנו``>"
+        }
+        message.channel.send(res.toString());
     }
 }
 
