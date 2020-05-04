6b411a5344272e0156db3dd812a433734854e2df
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-04 15:40:22 +0300

    fix

--- commands/SG.js
+++ commands/SG.js
@@ -14,29 +14,23 @@ class SG extends Command {
     async run(message, args, level) {
         let res = "https://www.sdarot.today/watch/92-stargate-sg-1-%D7%A1%D7%98%D7%90%D7%A8%D7%92%D7%99%D7%99%D7%98-%D7%90%D7%A1-%D7%92%D7%99-1";
         let popcorn = false;
-        if (args[0] != null && (args[1] != '-' || args[3] != null)) {
-            res = "שימוש שגוי בפקודה, שלח ללא פרמטרים או עם <עונה> - <פרק>"
+        if (!this.client.settings.has("lastEpisode")) {
+            this.client.settings.set("lastEpisode", res);
+        }
+        if (args[0] == null || (args[0] == "הקודם" && args[1] == null)) {
+            res = this.client.settings.get("lastEpisode");
+            popcorn = true;
+        }
+        else if (args[1] != '-' || args[3] != null) {
+            res = "שימוש שגוי בפקודה, שלח ללא פרמטרים או עם <עונה> - <פרק>";
+        }
+        else if (parseInt(args[0]).toString() == 'NaN' || parseInt(args[2]).toString() == 'NaN') {
+            res = "אורי מה זה השטויות האלה ששמת פה"
         }
         else {
-            if (args[0] == "הקודם") {
-                if (!this.client.settings.has("lastEpisode")) {
-                    this.client.lastEpisode.set("lastEpisode", res);
-                  }
-                else {
-                    res = this.client.settings.get("lastEpisode");
-                }
-                popcorn = true;
-            }
-            else if (parseInt(args[0]).toString() == 'NaN' || parseInt(args[2]).toString() == 'NaN') {
-                res = "אורי מה זה השטויות האלה ששמת פה"
-            }
-            else {
-                if (args[0] != null) {
-                    res += "-stargate-sg-1/season/" + args[0] + "/episode/" + args[2];
-                } 
-                popcorn = true;
-                this.client.settings.set("lastEpisode", res);
-            }
+            res += "-stargate-sg-1/season/" + args[0] + "/episode/" + args[2];
+            popcorn = true;
+
         }
         message.channel.send(res.toString());
         if (popcorn) {
