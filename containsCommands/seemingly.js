bbac69f5423fc3a55081d3b66be7e984d4bb80de
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-04 20:24:16 +0300

    Eitan curse test

--- containsCommands/seemingly.js
+++ containsCommands/seemingly.js
@@ -26,6 +26,31 @@ class Seemingly extends ContainsCommand {
             res = "תגיד לכאורה על זה, אתה חייב להגיד !"
         }
     }
+
+    if (!this.client.settings.has("EitanCurse")) {
+      this.client.lastThread.set("EitanCurse", 0);
+    }
+    let curse = this.client.settings.get("EitanCurse");
+    if (message.author.id == "300324644932681728") {
+      this.client.lastThread.set("EitanCurse", curse++);
+    }
+    if (this.client.settings.has("EitanCurse") % 4 == true) {
+      let PM = "";
+      let num = Math.floor(Math.random() * 4 + 1);
+      if (num == 4) {
+        PM = "איתן כדאי לך להיזהר ממני י'חצוף, למה אני שמתי את העין שלי עליך";
+      }
+      else if (num == 3) {
+        PM = "לא יודע מי חינך אותך איתן, אבל כדאי שתשמור על הפה שלך";
+      }
+      else if (num == 2) {
+        PM = "פעם הבאה אני לא אעבור על זה בשתיקה.";
+      }
+      else {
+        PM = "אני מקווה לטובתך שהיה לך סיבה טובה להגיד את זה";
+      }
+      message.author.send(PM);
+    }
     message.channel.send(res);
   }
 }
