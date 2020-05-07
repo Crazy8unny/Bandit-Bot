0b84a31f936ae9fc0a3a6deb18c20d88bd7788d9
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-07 22:37:08 +0300

    test

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -30,7 +30,7 @@ class ForumNotification {
     
     request.get(settings, function (err, res, data) {
       const $ = cheerio.load(data);
-      console.log($("tbody")[6].find("td"));
+      console.log($("tbody")[6].children("td"));
     });
   }
 }
