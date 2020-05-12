51c1010d1d77382e5964a8032372bb02460901d8
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-12 23:38:38 +0300

    test

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -8,15 +8,16 @@ const Discord = require('discord.js');
 // const cheerio = require('cheerio')
 const util = require('../util/utils');
 const JSDOM = require('jsdom').JSDOM;
-const iconv = require('iconv-lite')
+const iconv = require('iconv-lite');
 const $ = require("jquery");
+const request = require('request');
 
 class ForumNotification {
   static listen(client) {
     const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
     const prevName = client.lastThread.get("name");
+    const prevAuthor = client.lastThread.get("author");
     // console.log("test name: " + prevName);
-    const request = require('request');
     let settings = {
       "url": "https://lf2.co.il/forum/index.php",
       "method": "GET",
@@ -29,18 +30,30 @@ class ForumNotification {
       const jsdom = new JSDOM(iconv.decode(data, 'iso-8859-8'));
       const body = jsdom.window.document.getElementsByTagName("tbody")[6].getElementsByTagName("td")[1].getElementsByTagName("a");
       let name = body[body.length - 6].getElementsByTagName("b")[0];
-      if (name.innerHTML != prevName) {
+      let forum = body[body.length - 5].innerHTML;
+      let bodyWords = jsdom.window.document.getElementsByTagName("tbody")[6].getElementsByTagName("td")[1].innerHTML;
+      let position = bodyWords.search("תגובה אחרונה על ידי");
+      let author = bodyWords.substring(position, bodyWords.length);
+      position = author.search(",");
+      author = author.substring(20, position);
+
+      if (name.innerHTML != prevName || author != prevAuthor) {
         client.lastThread.set("name", name.innerHTML);
+        client.lastThread.set("author", author);
         let embed = {
           color: 0x0099ff,
           title: name.innerHTML,
-          url: "https://lf2.co.il" + name.href
+          url: "https://lf2.co.il" + name.href,
+          description: forum,
+          footer: {
+            text: author
+          }
         };
         console.log(name.innerHTML);
         client.channels.cache.find(c => c.id === '704981301572403211').send({ embed }).catch(console.error);
         client.channels.cache.find(c => c.id === '708218080815218748').send({ embed }).catch(console.error);
 
-        settings.url = body[body.length - 4].href
+        settings.url = "https://lf2.co.il" + body[body.length - 4].href
 
         // // console.log(link);
         // request.get(settings, function (err, res, dat) {
