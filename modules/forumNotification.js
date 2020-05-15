557a60c31ed519849881355c9fd442f09cc3d3d4
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-15 14:52:37 +0300

    test

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -71,7 +71,11 @@ class ForumNotification {
           const jsdom = new JSDOM(iconv.decode(data, 'iso-8859-8'));
           let table = jsdom.window.document.getElementsByClassName("forumline");
           console.log(table.length);
-          table = table[table.length - 1];
+          if (table.length <= 2) {
+            table = table[0];
+          } else {
+            table = table[table.length - 2];
+          }
           let MD = {};
           MD.avatar = table.getElementsByClassName("postdetails");
           MD.avatar = MD.avatar[MD.avatar.length - 3];
