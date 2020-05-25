6108e718d86bac11d4ed4511aeaae17e280b3cbd
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-25 21:23:39 +0300

    fix

--- index.js
+++ index.js
@@ -17,17 +17,21 @@ class BanditBot extends Client {
   constructor (options) {
     super(options);
 
+    console.log(process.env.private_key_id);
+    console.log(process.env.private_key);
     console.log(process.env.client_email);
+    console.log(process.env.client_id);
+
 
     // Init firebase
     admin.initializeApp({
       credential: admin.credential.cert({
         "type": "service_account",
         "project_id": "bandit-bot-db",
-        "private_key_id": process.env.private_key_id.toString(),
-        "private_key": process.env.private_key.toString(),
-        "client_email": process.env.client_email.toString(),
-        "client_id": process.env.client_id.toString(),
+        "private_key_id": process.env.private_key_id,
+        "private_key": process.env.private_key,
+        "client_email": process.env.client_email,
+        "client_id": process.env.client_id,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
