76b8569da9ed6e67c2cde31c2a9c42a32206cf52
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-26 22:11:16 +0300

    test

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -57,7 +57,7 @@ class ForumNotification {
           //find new user details
           let newUserTable = jsdom.window.document.getElementsByTagName("tbody");
           console.log(newUserTable.length);
-          newUserTable = newUserTable[newUserTable.length - 4];
+          newUserTable = newUserTable[newUserTable.length - 2];
           newUserTable = newUserTable.getElementsByTagName("tr");
           console.log(newUserTable.length);
           newUserTable = newUserTable[newUserTable.length - 3];
