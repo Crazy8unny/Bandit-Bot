c991c25de2f1c3cafdd49b5a9fa8236ffb834c73
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-27 15:30:29 +0300

    fix

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -153,7 +153,7 @@ class ForumNotification {
           }
 
           function sendEmbed (embed) {
-            this.db.collection("lastThread").doc("Servers").get().then(Servers => {
+            client.db.collection("lastThread").doc("Servers").get().then(Servers => {
               Servers.keys(servers).forEach(function(id){
                 client.channels.cache.find(c => c.id === servers.id).send({ embed }).catch(console.error);
               });       
