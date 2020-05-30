1a1d3c2327b6a308ab8278f1f45188da3ea41740
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-05-30 20:53:17 +0300

    test

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -83,10 +83,10 @@ class ForumNotification {
 
           // check if there is a new user
           if (newUser != prevNewUser) {
-            newUser = "**" + newUser + "**";
             client.lastThread.set({ name: prevName, author: prevAuthor, commentsNumber: prevNumber, newUser: newUser });
+            newUser = "**" + newUser + "**";
             let embed2 = {
-              title: `${newUser} הצטרף לפורום !!!!!1111`,
+              title: `${newUser} הצטרף לפורום !!!!!`,
               color: "#FF7519"
             };
             sendEmbed(embed2);
