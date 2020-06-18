d15587c695dd928b04bd6489e15d2865e01228ce
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-06-18 19:04:59 +0300

    add return

--- modules/forumNotification.js
+++ modules/forumNotification.js
@@ -171,6 +171,7 @@ class ForumNotification {
                       }
                     }
                   }
+                  return embed;
                 }
               }
             });
