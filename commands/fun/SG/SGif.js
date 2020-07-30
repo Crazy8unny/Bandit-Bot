cc578bbad6f567ee3a75632f9b2c0a2ea8934478
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2020-07-30 18:18:41 +0300

    this common

---
+++ commands/fun/SG/SGif.js
@@ -0,0 +1,34 @@
+const Command = require("../../../base/Command.js");
+const Discord = require('discord.js');
+
+class SGif extends Command {
+    constructor(client) {
+        super(client, {
+            name: "הזה",
+            description: "מדפיס את השם של הסדרה הזאת נו",
+            usage: "הזה נו",
+            aliases: [],
+            category: "פנאי"
+        });
+    }
+
+    async run(message, args, level) {
+        if (args[0] == null || args[1] != null) {
+            message.channel.send("שימוש שגוי בפקודה, שלח `!עזרה הזה` על מנת לקבל מידע מלא על הפקודה");
+        }
+        else if (args[0] == "נו") {
+            try {
+                let embed = new Discord.MessageEmbed()
+                    .setImage("https://i.imgur.com/0gYpI9t.gif")
+                    .setColor('#1E2023');
+                message.channel.send(embed);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+}
+
+
+
+module.exports = SGif;
