887f3521877d3c04fc083dad2dcaa175ef6ce420
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2021-02-02 19:59:35 +0200

    add inspire

---
+++ commands/fun/inspire.js
@@ -0,0 +1,38 @@
+const Command = require("../../base/Command.js");
+const Discord = require('discord.js');
+
+class Inspire extends Command {
+  constructor(client) {
+    super(client, {
+      name: "השראה",
+      description: "מביא לך קצת השראה על חשבנון הבית אחי",
+      usage: "השראה",
+      aliases: [],
+      category: "פנאי"
+    });
+  }
+
+  async run(message, args, level) {
+    let url = "https://inspirobot.me/api?generate=true";
+    let settings = {
+      "url": url,
+      "method": "GET",
+      "encoding": null
+    }
+    request.get(settings, function (error, response, data) {
+      try {
+        let embed = new Discord.MessageEmbed()
+          .setImage("data")
+          .setAuthor(message.author.username, message.author.displayAvatarURL())
+          .setColor('#1E2023');
+        message.channel.send(embed);
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  }
+}
+
+
+
+module.exports = Inspire;
